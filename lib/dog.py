39ba4f21769189cc7f439ef9eb0913f6c6d089c4
Your Name <you@example.com>
2025-08-07 01:18:25 -0700

    complete assignment

--- lib/dog.py
+++ lib/dog.py
@@ -8,8 +8,35 @@ APPROVED_BREEDS = [
     "Beagle",
     "French Bulldog",
     "Pug",
-    "Pointer"
+    "Pointer",
+    "Mutt"  # Added Mutt to approved breeds
 ]
 
 class Dog:
-    pass
+    def __init__(self, name="Unnamed", breed="Mutt"):
+        self.name = name
+        self.breed = breed
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and 1 <= len(name) <= 25:
+            self._name = name
+        else:
+            print("Name must be string between 1 and 25 characters.")
+            self._name = None
+
+    @property
+    def breed(self):
+        return self._breed
+
+    @breed.setter
+    def breed(self, breed):
+        if breed in APPROVED_BREEDS:
+            self._breed = breed
+        else:
+            print("Breed must be in list of approved breeds.")
+            self._breed = None
